56ab3298d0869d7ec85e3e0b75ef393dd1f6480f
srithep444 <srithep555@gmail.com>
2018-09-26 23:30:42 +0700

    Finish
    เสร็จแล้วว

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { Card, Button, CardImg, CardTitle, CardText, CardGroup,
+ CardSubtitle, CardBody } from 'reactstrap';
+import { Badge } from 'reactstrap';
+import './Content.css';
 
-export default () => (
-  <div className="has-text-centered">
-    <section class="hero is-danger">
-      <div className="container">
-        <h1 className="title">About</h1>
-        <p>บล็อคเล่าเรื่องราวเกี่ยวกับ Code / Programming ต่างๆ</p>
-        <p>ทั้ง JavaScript, Node.js, React.js รวมถึง Stack ที่เจ้าของบล็อคสนใจ</p>
-      </div>
-    </section>
-  </div>
-)
+const Content = (props) => {
+  return (
+    <div class = "Backgound">
+    <center><h1><Badge color="secondary">ผู้พัฒนา</Badge></h1></center>
+    <CardGroup>
+      <Card body inverse color="primary">
+        <CardImg top width="100%" src="https://www.picz.in.th/images/2018/09/26/hWNvAu.jpg" />
+        <CardBody>
+            <CardText>นายธิปมนัส รัตนนุพงค์</CardText>
+            <CardText>รหัสนักศึกษา 5910110148</CardText>
+            <a href="https://www.facebook.com/thipmanus.rattananupong">
+            <Button color="info" size="lg" block >Facebook</Button>
+            </a>
+        </CardBody>
+        </Card>
+        <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333'}}>
+        <CardImg top width="100%" src="https://www.picz.in.th/images/2018/09/26/hWBIvb.jpg"  />
+        <CardBody>
+          <CardText>นายวรปรัชญ์ รัตนะ</CardText>
+          <CardText>รหัสนักศึกษา 5910110288</CardText>
+          <a href="https://www.facebook.com/pond.long">
+            <Button color="info" size="lg" block >Facebook</Button>
+          </a>
+        </CardBody>
+      </Card>
+      <Card body inverse color="danger">
+        <CardImg top width="100%" src="https://www.picz.in.th/images/2018/09/26/hWYS5l.jpg" />
+        <CardBody>
+            <CardText>นายศรีเทพ วิทยาพันธ์ประชา</CardText>
+            <CardText>รหัสนักศึกษา 5910110669</CardText>
+            <a href="https://www.facebook.com/PeachGenZ">
+            <Button color="info" size="lg" block >Facebook</Button>
+            </a>
+        </CardBody>
+      </Card>
+    </CardGroup>
+    </div>
+  );
+};
+
+export default Content;
